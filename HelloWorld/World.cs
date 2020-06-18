b388b07abc7c994a64ff0e8de767dedcfab5602d
aust3213 <aust3213@AULH4MHBG2>
2020-06-18 11:20:38 +1000

    Add project files.

---
+++ HelloWorld/World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class World
+    {
+        public string SayHelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloWorldUnitTest/HelloWorldUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldUnitTest
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void SayHelloWorld()
+        {
+            var world = new World();
+            Assert.AreEqual("Hello World", world.SayHelloWorld());
+        }
+    }
+}
